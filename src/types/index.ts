97833a35a98fe8604ad00f1acb45f1cadc462987
Pablo Poggio <pablopoggiog@gmail.com>
2022-06-25 13:02:16 -0300

    Create RawProduct type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,18 @@
 export type ProductType = string; // TO DO: change to specific product types
 
+export interface RawProduct {
+  name: string;
+  type: ProductType;
+  price: number;
+  image: string;
+  head: string;
+  tail: string;
+}
+
 export interface Product {
   name: string;
   type: ProductType;
   price: number;
   image: string;
+  id: string;
 }
