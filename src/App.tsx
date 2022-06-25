e5c763f865fdd99911774caa6652a0c149a5227b
Pablo Poggio <pablopoggiog@gmail.com>
2022-06-25 17:19:55 -0300

    Redirect to /products from /

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "./components";
-import { Home, Products, Checkout } from "./pages";
+import { Products, Checkout } from "./pages";
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route index element={<Navigate replace to="/products" />} />
       <Route path="/products" element={<Products />} />
       <Route path="/checkout" element={<Checkout />} />
     </Routes>
