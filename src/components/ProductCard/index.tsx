4fe354a8c06f731fd4534ac060313adc9eddd9cc
Pablo Poggio <pablopoggiog@gmail.com>
2022-06-25 00:08:37 -0300

    Create types directory

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,16 +1,9 @@
 import { FC } from "react";
 import { VStack, Text, GridItem, Button, Image } from "@chakra-ui/react";
 import CartIcon from "assets/cart-icon.png";
+import { Product } from "types";
 
-type ProductType = string; // TO DO: change to specific product types
-
-interface ProductCardProps {
-  name: string;
-  type: ProductType;
-  price: number;
-}
-
-export const ProductCard: FC<ProductCardProps> = ({ name, type, price }) => {
+export const ProductCard: FC<Product> = ({ name, type, price }) => {
   return (
     <GridItem borderRadius="md" p={5} w="full" h="250" bg="blue.500">
       <VStack h="full" justify="space-around">
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type ProductType = string; // TO DO: change to specific product types
+
+export interface Product {
+  name: string;
+  type: ProductType;
+  price: number;
+}
