f55118777f48a3548b8f9902bda7556953f555a0
Pablo Poggio <pablopoggiog@gmail.com>
2022-06-25 21:19:55 -0300

    Add total price to checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,4 +1,10 @@
-import { Flex, useColorModeValue, VStack } from "@chakra-ui/react";
+import {
+  Flex,
+  useColorModeValue,
+  VStack,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 import { useAppSelector } from "hooks/reduxHooks";
 
 import { CartItem } from "components";
@@ -7,7 +13,7 @@ import { cartReducer } from "state/slices/cart/slice";
 export const Checkout = () => {
   const backgroundColor = useColorModeValue("gray.400", "gray.700");
 
-  const { cartList } = useAppSelector(cartReducer);
+  const { cartList, totalPrice } = useAppSelector(cartReducer);
 
   return (
     <Flex
@@ -18,14 +24,23 @@ export const Checkout = () => {
       minH="85vh"
     >
       <VStack gap={8} w="full">
-        {cartList?.map(({ product, amount }, index) => (
-          <CartItem
-            key={product.id}
-            product={product}
-            amount={amount}
-            isTheLastItem={index === cartList.length - 1}
-          />
+        {cartList?.map(({ product, amount }) => (
+          <CartItem key={product.id} product={product} amount={amount} />
         ))}
+
+        <Flex
+          direction={{ base: "column", lg: "row" }}
+          align="center"
+          justify="flex-end"
+          w="full"
+          gap={5}
+        >
+          <Text fontSize="lg" fontWeight="bold">
+            Total: ${totalPrice}
+          </Text>
+
+          <Button>Finish Purchase</Button>
+        </Flex>
       </VStack>
     </Flex>
   );
