f867a20d48e63a123876117e21b3746ff358f51b
Pablo Poggio <pablopoggiog@gmail.com>
2022-06-25 22:55:49 -0300

    Enhance cards animation

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -7,7 +7,6 @@ import {
   Image,
   Flex,
   useColorModeValue,
-  Icon,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useAppDispatch } from "hooks/reduxHooks";
@@ -15,8 +14,7 @@ import { addProduct } from "state/slices/cart/slice";
 import { ReactComponent as CartIcon } from "assets/cart-icon.svg";
 import { Product } from "types";
 
-// const AnimatedImage = motion(Image);
-const AnimatedGridItem = motion(GridItem);
+const AnimatedImage = motion(Image);
 
 export const ProductCard: FC<Product> = ({ name, type, price, image, id }) => {
   const [buttonText, setButtonText] = useState<string>();
@@ -34,18 +32,27 @@ export const ProductCard: FC<Product> = ({ name, type, price, image, id }) => {
   };
 
   return (
-    <AnimatedGridItem
+    <GridItem
       rounded="md"
       w="full"
       h="250"
       bg={imageBackgroundColor}
       overflow="hidden"
-      animate={{ opacity: [0, 0.9, 1], scale: [1, 1.05, 1] }}
-      transition={{ duration: 0.7, time: [0, 0.95, 1] }}
     >
       <VStack h="full" justify="space-between">
         <Flex h={160} w="full" m="auto" position="relative">
-          <Image src={image} h="85%" m="auto" />
+          <AnimatedImage
+            src={image}
+            h="85%"
+            m="auto"
+            layout
+            animate={{
+              opacity: [0, 0.5, 1],
+              scale: [0.1, 1.1, 1],
+              x: [-30, 30, 0],
+            }}
+            transition={{ duration: 0.7, ease: "easeInOut" }}
+          />
           <Text
             fontSize={["xs", "sm"]}
             m={0}
@@ -94,6 +101,6 @@ export const ProductCard: FC<Product> = ({ name, type, price, image, id }) => {
           </Flex>
         </VStack>
       </VStack>
-    </AnimatedGridItem>
+    </GridItem>
   );
 };
